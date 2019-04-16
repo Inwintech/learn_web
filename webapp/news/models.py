0003203d9c5fcc4dc66c0ef75386afcb37485e45
Inwintech <igor.margolin@mail.ru>
2019-04-16 16:22:42 +0300

    8 lesson

--- webapp/news/models.py
+++ webapp/news/models.py
@@ -6,6 +6,7 @@ class News(db.Model):
     url = db.Column(db.String, unique=True, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
+    email = db.Column(db.Sring(50), unique=True)
 
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
